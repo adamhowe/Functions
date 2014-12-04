40e51deafe6aebee4a691b41d5216f5d5a3cc424
Adam-Howe <40769@longroad.ac.uk>
2014-12-04 12:11:42 +0000

    Revision Exercise

---
+++ Revision Exercise 4.py
@@ -0,0 +1,37 @@
+#Adam Howe
+#Revision Exercise 4
+#04/12/2014
+
+def convert(temp):
+    temp_two = temp - 32
+    celsius = temp * 0.5555555555555556
+    print()
+    print("{0} degrees fahrenheit = {1} degrees celsius".format(temp,celsius))
+
+temp = int(input("Enter the degrees in fahrenheit that you want converted (a whole number): "))
+convert = convert(temp) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
